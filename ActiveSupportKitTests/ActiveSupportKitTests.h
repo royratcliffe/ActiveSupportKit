f1d4ce33dd9a733d485186b46277321ab7a1d2a2
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-08-04 06:26:45 +0100

    Cocoa framework, initial commit

---
+++ ActiveSupportKitTests/ActiveSupportKitTests.h
@@ -0,0 +1,13 @@
+//
+//  ActiveSupportKitTests.h
+//  ActiveSupportKitTests
+//
+//  Created by Roy Ratcliffe on 04/08/2011.
+//  Copyright 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ActiveSupportKitTests : SenTestCase
+
+@end
---
+++ ActiveSupportKitTests/ActiveSupportKitTests.m
@@ -0,0 +1,32 @@
+//
+//  ActiveSupportKitTests.m
+//  ActiveSupportKitTests
+//
+//  Created by Roy Ratcliffe on 04/08/2011.
+//  Copyright 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import "ActiveSupportKitTests.h"
+
+@implementation ActiveSupportKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ActiveSupportKitTests");
+}
+
+@end
