56fc28e12a8f67c71e8d97f73c4ca0de547241fa
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-02-13 12:06:30 +0000

    Check JSON objects for circular references

--- ActiveSupportKit/ASJSON.h
+++ ActiveSupportKit/ASJSON.h
@@ -31,3 +31,31 @@ NSData *ASJSONEncodeToData(id object, NSError *__autoreleasing *outError);
 // string
 id ASJSONDecodeFromString(NSString *string, NSError *__autoreleasing *outError);
 NSString *ASJSONEncodeToString(id object, NSError *__autoreleasing *outError);
+
+/*!
+ * @result YES if the JSON object contains at least one circular reference; NO
+ * if it contains no circular references.
+ */
+BOOL ASCheckJSONObjectForCircularReferences(id object);
+
+/*!
+ * @brief Walks a JSON object and also checks the given JSON object for circular
+ * references, failing if circular.
+ * @param object The JSON object to walk, where objects have one of three types:
+ * array, dictionary or primitive. Primitives include strings, numbers and
+ * date-times. The root JSON object can be any one of these three, as can array
+ * elements and dictionary objects. Dictionary keys can be strings only.
+ * @result Answers an array of unique objects extracted by walking the JSON @a
+ * object. The array contains all primitives, all arrays and their elements, and
+ * all dictionaries and their values. The array does @em not contain dictionary
+ * keys. You can extract dictionary keys from the result by picking out the
+ * dictionaries. Answers @c nil if the JSON object contains circular references.
+ * @details Walks the JSON object, adding each object to the resulting array
+ * one-by-one unless it encounters an object already seen. Checking for circular
+ * references uses the <code>-[NSArray firstObjectCommonWithArray:]</code>
+ * method. If using an array is not efficient, the implementation could improve
+ * by using a set rather than an array. As a disadvantage in that case, the
+ * ordering would become undefined. Dictionaries already make their ordering
+ * undefined.
+ */
+NSArray *ASAllObjectsInJSONObject(id object);
--- ActiveSupportKit/ASJSON.m
+++ ActiveSupportKit/ASJSON.m
@@ -44,3 +44,41 @@ NSString *ASJSONEncodeToString(id object, NSError *__autoreleasing *outError)
 	NSData *data = ASJSONEncodeToData(object, outError);
 	return data ? [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] : nil;
 }
+
+BOOL ASCheckJSONObjectForCircularReferences(id object)
+{
+	return ASAllObjectsInJSONObject(object) != nil;
+}
+
+NSArray *ASAllObjectsInJSONObject(id object)
+{
+	// Importantly, do not use an enumerator to iterate the array elements. The
+	// iterator will modify the array; the count of objects increases at
+	// iterations where the object is a non-empty array or dictionary. The array
+	// has a two-fold function. First, to unwrap the arrays and
+	// dictionaries. Second, to collect the array and dictionary contents for
+	// checking; JSON objects should never contain circular references.
+	NSMutableArray *objects = [NSMutableArray arrayWithObject:object];
+	for (NSUInteger index = 0; index < [objects count]; index++)
+	{
+		id object = [objects objectAtIndex:index];
+		if ([object isKindOfClass:[NSArray class]])
+		{
+			if ([objects firstObjectCommonWithArray:(NSArray *)object])
+			{
+				return nil;
+			}
+			[objects addObjectsFromArray:(NSArray *)object];
+		}
+		else if ([object isKindOfClass:[NSDictionary class]])
+		{
+			NSArray *values = [(NSDictionary *)object allValues];
+			if ([objects firstObjectCommonWithArray:values])
+			{
+				return nil;
+			}
+			[objects addObjectsFromArray:values];
+		}
+	}
+	return [objects copy];
+}
