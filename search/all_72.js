b424e7fbe638c4828b60b6bd9386551bbdcda1de
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2013-04-19 10:44:04 +0100

    Updated GitHub pages using Doxygen 1.8.3.1

--- search/all_72.js
+++ search/all_72.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['readme_2emd',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
   ['replacematchesinstring_3areplacementstringforresult_3a',['replaceMatchesInString:replacementStringForResult:',['../category_n_s_regular_expression_07_active_support_08.html#a8110e46ae8a7baa2905913f9a19863a9',1,'NSRegularExpression(ActiveSupport)']]]
 ];
--- search/pages_61.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['active_20support_20kit',['Active Support Kit',['../index.html',1,'']]]
-];
